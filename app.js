c7e2498b83ecda9dd0bff048cfe9caffec2199c9
Dhruv Athaide <athaidedhruv@gmail.com>
2023-10-07 11:55:32 +0530

    Updated Form Submittion Optimization

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Theme Button Function
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function () {
@@ -12,17 +13,52 @@
     })
 })();
 
-var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbwTVyK2PUBa1wys5WBpD-H81tutzxw6-463RWPFkmtv8OVoN573TEZJYpXESCkm9HlHWw/exec'
 
-$('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-  );
-})
+//Newer Google Sheet App Script Connection
+document.addEventListener('DOMContentLoaded', function () {
+  var form = document.getElementById('myform');
+  var url = 'https://script.google.com/macros/s/AKfycbwTVyK2PUBa1wys5WBpD-H81tutzxw6-463RWPFkmtv8OVoN573TEZJYpXESCkm9HlHWw/exec';
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var formData = new FormData(form);
+
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+    .then(function (response) {
+      if (response.ok) {
+        //Message to alert whether Form is succesfully Submitted
+        alert('Form submitted successfully!');
+      } else {
+        throw new Error('Network response was not ok.');
+      }
+    })
+    .catch(function (error) {
+      //Error Message if my Form Submittion Fails
+      alert('Error submitting the form. Please try again.');
+      console.error('Error:', error);
+    });
+  });
+});
+
+
+
+
+//Older Google Sheet App Script Connection
+// var $form = $('form#test-form'),
+//     url = 'https://script.google.com/macros/s/AKfycbwTVyK2PUBa1wys5WBpD-H81tutzxw6-463RWPFkmtv8OVoN573TEZJYpXESCkm9HlHWw/exec'
+
+// $('#submit-form').on('click', function(e) {
+//   e.preventDefault();
+//   var jqxhr = $.ajax({
+//     url: url,
+//     method: "GET",
+//     dataType: "json",
+//     data: $form.serializeObject()
+//   }).success(
+//     // do something
+//   );
+// })
