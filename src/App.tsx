87933d8210d9bcf5a8f0d003c38a649766b13a7d
Dhruv Athaide <athaidedhruv@gmail.com>
2025-07-10 19:39:34 +0530

    Removed Unecessary Imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from './context/ThemeContext';
